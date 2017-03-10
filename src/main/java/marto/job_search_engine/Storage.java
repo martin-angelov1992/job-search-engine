ae6859f7deb037fd271cce1ecd115d030b9488e9
Martin Angelov <martin_angelov@epam.com>
2017-03-10 19:03:37 +0200

    partly implement adding jobs to elasticsearch

--- src/main/java/marto/job_search_engine/Storage.java
+++ src/main/java/marto/job_search_engine/Storage.java
@@ -1,13 +1,18 @@
 package marto.job_search_engine;
 
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Date;
 import java.util.Map;
 
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import static org.elasticsearch.common.xcontent.XContentFactory.*;
 
 public class Storage {
 	private TransportClient client;
@@ -23,7 +28,19 @@ public class Storage {
 	}
 
 	public void processJobWithKnownFormat(Map<String, String> categoryTypes, String company, String jobTitle, String text) {
-		
+		try {
+			XContentBuilder builder = jsonBuilder().startObject();
+
+			
+                .field("user", "kimchy");
+            builder.endObject();
+			IndexResponse response = client.prepareIndex("jobs", company)
+			        .setSource(builder)
+			        .get();
+		} catch (IOException e) {
+			System.err.println("Unable to create document.");
+			e.printStackTrace();
+		}
 	}
 
 	public void processJobWithUnknownFormat(String text) {
